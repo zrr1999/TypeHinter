4bd91d0fe2016ec79626147da008611a8a97a975
zrr1999 <2742392377@qq.com>
2022-12-01 20:39:20 +0800

    opt utils

--- tools/utils.py
+++ tools/utils.py
@@ -21,19 +21,36 @@ def load_parsed_yaml(path: str) -> dict[str, tuple[list, list]]:
     return func_list
 
 
-def dump_func(name: str, inputs: list, outputs: list, defaults: list, hint=True):
+def type_mapping(origin_type: str):
+    if origin_type.endswith("[]"):
+        return f"list[{origin_type[:-2]}]"
+    else:
+        return origin_type
+
+
+def dump_func(name: str, inputs: list, outputs: list, defaults: list, end_arg: str|tuple[str, str], hint: bool = True):
     if hint:
         inputs = [
-            f"{n}: {t}" for t, n in inputs
+            f"{n}: {type_mapping(t)}" for t, n in inputs
         ]
         for i, v in enumerate(defaults, 1):
             inputs[len(inputs) - i] += f" = {v}"
         inputs = ", ".join(inputs)
         if len(outputs) == 1:
-            outputs = f"{outputs[0]}"
+            outputs = f"{type_mapping(outputs[0])}"
         else:
-            outputs = f"list{outputs}"
-        return f"def {name}({inputs}, name: str = None) -> {outputs}: \n"
+            outputs = ", ".join(type_mapping(t) for t in outputs)
+            outputs = f"tuple[{outputs}]"
+
+        if not isinstance(end_arg, str):
+            if end_arg[0] == "name":
+                end_arg = "name: str = None"
+            else:
+                if end_arg[1] is not None:
+                    end_arg = f"{end_arg[0]}: {type(end_arg[1]).__name__} = {end_arg[1]}"
+                else:
+                    end_arg = f"{end_arg[0]}={end_arg[1]}"
+        return f"def {name}({inputs}, {end_arg}) -> {outputs}: \n"
     else:
         inputs = ", ".join([
             n for t, n in inputs
@@ -54,7 +71,7 @@ def get_source_func(module: ast.Module):
         if isinstance(node, ast.FunctionDef):
             res.append({
                 "name": node.name,
-                "args": [arg.arg for arg in node.args.args[:-1]],  # end args is 'name'
+                "args": [arg.arg for arg in node.args.args],
                 "defaults": [default.value if isinstance(default, ast.Constant) else NotImplementedError
                              for default in node.args.defaults],
                 "lineno": node.lineno
@@ -64,6 +81,7 @@ def get_source_func(module: ast.Module):
 
 def replace_paddle(source_path: str, yaml_funcs: dict[str, tuple[list, list]], inplace=True):
     assert inplace  # TODO: not inplace mode
+
     not_replaced = []
     replaced = []
     with open(source_path, 'r') as file:
@@ -75,7 +93,7 @@ def replace_paddle(source_path: str, yaml_funcs: dict[str, tuple[list, list]], i
     for func in source_funcs:
         yaml_func = yaml_funcs.get(func["name"])
 
-        if yaml_func is None or (len(func["args"]) != len(yaml_func[0])):
+        if yaml_func is None or (len(func["args"]) != len(yaml_func[0]) + 1):
             # print(func["name"], len(func["args"]))
             not_replaced.append((func, yaml_func))
         else:
@@ -87,12 +105,18 @@ def replace_paddle(source_path: str, yaml_funcs: dict[str, tuple[list, list]], i
                     print(func["name"], a, ya)
                     break
             if matched:
-                lines[func["lineno"] - 1] = dump_func(func["name"], *yaml_func, func["defaults"])
-
-                # print(lines[func["lineno"]])
+                end_arg = (func["args"][-1], func["defaults"][-1]) if func["defaults"] else func["args"][-1]
+                lines[func["lineno"] - 1] = dump_func(func["name"], *yaml_func, func["defaults"][:-1],
+                                                      end_arg=end_arg)
                 replaced.append((func, yaml_func))
 
     with open(source_path, mode="w") as file:
+        # import Tensor
+        lines.insert(13,
+                     "from __future__ import annotations\n"
+                     "from typing import TYPE_CHECKING\n"
+                     "if TYPE_CHECKING:\n"
+                     "    from .tensor import Tensor\n")
         file.write("".join(lines))
 
     return replaced, not_replaced
