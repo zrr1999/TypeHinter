1b90b984cf7652682d7b9c0401e2eadc6fcb49e9
zrr1999 <2742392377@qq.com>
2022-12-01 17:50:53 +0800

    add python source parse

--- demo.py
+++ demo.py
@@ -3,30 +3,77 @@
 # @Time : 2022/11/27 16:52
 # @Author : Rongrui Zhan
 # @desc : 本代码未经授权禁止商用
+from __future__ import annotations
 import os
-from tools.utils import replace_func, load_api_yaml
+import subprocess
+import typing
+import ast
+import numpy as np
+from tools.utils import load_parsed_yaml, get_source_func, dump_func
 
 if not os.path.exists("generated/tensor"):
     if not os.path.exists("generated"):
         os.mkdir("generated")
     os.mkdir("generated/tensor")
 
-yaml_paths = ['tools/yaml/strings_ops.yaml', 'tools/yaml/sparse_backward.yaml',
-              'tools/yaml/backward.yaml', 'tools/yaml/sparse_ops.yaml',
-              'tools/yaml/legacy_ops.yaml', 'tools/yaml/legacy_backward.yaml', 'tools/yaml/ops.yaml']
-func_list = []
+yaml_paths = ['tools/yaml/parsed_apis/api.parsed.yaml',
+              'tools/yaml/parsed_apis/legacy_api.parsed.yaml',
+              'tools/yaml/parsed_apis/backward_api.parsed.yaml',
+              'tools/yaml/parsed_apis/legacy_backward_api.parsed.yaml']
+yaml_funcs = {}
 for yaml_path in yaml_paths:
-    func_list.extend(load_api_yaml(yaml_path))
+    yaml_funcs.update(load_parsed_yaml(yaml_path))
 
-source_paths = ['source/tensor/manipulation.py', 'source/tensor/attribute.py', 'source/tensor/creation.py', 'source/tensor/einsum.py', 'source/tensor/__init__.py', 'source/tensor/random.py', 'source/tensor/logic.py', 'source/tensor/tensor.py', 'source/tensor/ops.py', 'source/tensor/layer_function_generator.py', 'source/tensor/linalg.py', 'source/tensor/stat.py', 'source/tensor/to_string.py', 'source/tensor/search.py', 'source/tensor/math.py', 'source/tensor/array.py']
+source_paths = ['source/tensor/manipulation.py', 'source/tensor/attribute.py', 'source/tensor/creation.py',
+                'source/tensor/einsum.py', 'source/tensor/random.py',
+                'source/tensor/logic.py', 'source/tensor/ops.py',
+                'source/tensor/layer_function_generator.py', 'source/tensor/linalg.py', 'source/tensor/stat.py',
+                'source/tensor/to_string.py', 'source/tensor/search.py', 'source/tensor/math.py',
+                'source/tensor/array.py']
+
+
+all_not_replaced = {}
+all_not_replaced_num = 0
+all_replaced_num = 0
 for source_path in source_paths:
-    with open(source_path, 'r') as source:
-        source = source.read()
-    for func in func_list:
-        source = replace_func(source, *func)
-    with open(source_path.replace("source", "generated"), mode="w") as stub_file:
-        stub_file.write(source)
+    not_replaced = []
+    replaced = []
+    with open(source_path, 'r') as file:
+        lines = file.readlines()
+        source = "".join(lines)
+        module = ast.parse(source)
+        source_funcs = get_source_func(module)
 
-# import paddle
-#
-# print(paddle.add().)
+    for func in source_funcs:
+        yaml_func = yaml_funcs.get(func["name"])
+
+        if func["name"] == "divide":
+            print(1)
+
+        if yaml_func is None or (len(func["args"]) != len(yaml_func[0])):
+            # print(func["name"], len(func["args"]))
+            not_replaced.append((func, yaml_func))
+        else:
+            matched = True
+            for a, ya in zip(func["args"], yaml_func[0]):
+                if a != ya[1]:
+                    not_replaced.append((func, yaml_func))
+                    matched = False
+                    print(func["name"], a, ya)
+                    break
+            if matched:
+                lines[func["lineno"] - 1] = dump_func(func["name"], *yaml_func, func["defaults"])
+
+                # print(lines[func["lineno"]])
+                replaced.append((func, yaml_func))
+
+    # if len(not_replaced):
+    all_not_replaced[source_path] = (f"{len(replaced)}/{len(replaced) + len(not_replaced)}")
+    all_not_replaced_num += len(not_replaced)
+    all_replaced_num += len(replaced)
+    with open(source_path.replace("source", "generated"), mode="w") as stub_file:
+        stub_file.write("".join(lines))
+
+print(all_not_replaced)
+print(all_not_replaced_num)
+print(all_replaced_num)
--- tools/utils.py
+++ tools/utils.py
@@ -1,56 +1,62 @@
 from __future__ import annotations
+
+import ast
 import re
 import yaml
 
 
-def load_api_yaml(path: str, use_pyparsing:bool=False) -> list[tuple[str, list, list]]:
-    if use_pyparsing:
-        raise NotImplementedError
-    else:
-        match_args = re.compile(r"\(?([_A-z0-9]+)\s+"
-                                r"([_()A-z0-9]+)\s*"
-                                r"(?==\s+([_A-z0-9]+))?\)?")
-        match_outputs = re.compile(r"([_A-z0-9]+)\s*"
-                                   r"(?!\([_A-z0-9]+\))?")
-
-        func_list = []
-        with open(path, 'r') as f:
-            api_list = yaml.load(f, Loader=yaml.FullLoader)
-        for api in api_list:
-            api_name = api.get("backward_op", api.get("op"))
-            api_args = api["args"]
-            api_outs = api["output"]
-            # api_name = api.get("backward_op", api.get("op"))
-            # api_args = api.get("inputs", api.get("args"))
-            # api_outs = api.get("outputs", api.get("output"))
-            args = []
-            for e in api_args.split(","):
-                match_res = match_args.findall(e)
-                if match_res:
-                    args.append(match_res[0][:2])
-            outs = [match_outputs.findall(e)[0] for e in api_outs.split(",")]
-            func_list.append((api_name, args, outs))
+def load_parsed_yaml(path: str) -> dict[str, tuple[list, list]]:
+    func_list = {}
+    with open(path, 'r') as f:
+        api_list = yaml.load(f, Loader=yaml.FullLoader)
+    for api in api_list:
+        api_name = api["name"]
+        if api_name == "transpose":
+            print(api)
+        api_inputs = api["inputs"]
+        api_outputs = api["outputs"]
+        inputs = map(lambda inp: (inp["typename"], inp["name"]), api_inputs)
+        outputs = map(lambda out: out["typename"], api_outputs)
+        func_list[api_name] = (list(inputs), list(outputs))
     return func_list
 
 
-def dump_func(name: str, inputs: list, outputs: list, hint=False):
+def dump_func(name: str, inputs: list, outputs: list, defaults:list, hint=True):
     if hint:
-        inputs = ", ".join([
+        inputs = [
             f"{n}: {t}" for t, n in inputs
-        ])
+        ]
+        for i, v in enumerate(defaults, 1):
+            inputs[len(inputs)-i] += f" = {v}"
+        inputs = ", ".join(inputs)
         if len(outputs) == 1:
             outputs = f"{outputs[0]}"
         else:
-            outputs = f"typing.List{outputs}"
-        return f"def {name}({inputs}, name: str = None) -> {outputs}: "
+            outputs = f"list{outputs}"
+        return f"def {name}({inputs}, name: str = None) -> {outputs}: \n"
     else:
         inputs = ", ".join([
             n for t, n in inputs
         ])
-        return f"def {name}({inputs}, name=None):"
+        return f"def {name}({inputs}, name=None):\n"
 
 
-def replace_func(source: str, name: str, inputs: list, outputs: list):
+def replace_func(source: str, name: str, inputs: list, outputs: list, lineno: int):
     old_func = dump_func(name, inputs, outputs)
     new_func = dump_func(name, inputs, outputs, hint=True)
+    # print(old_func, new_func)
     return source.replace(old_func, new_func)
+
+
+def get_source_func(module: ast.Module):
+    res = []
+    for node in module.body:
+        if isinstance(node, ast.FunctionDef):
+            res.append({
+                "name": node.name,
+                "args": [arg.arg for arg in node.args.args[:-1]],  # end args is 'name'
+                "defaults": [default.value if isinstance(default, ast.Constant) else NotImplementedError
+                             for default in node.args.defaults],
+                "lineno": node.lineno
+            })
+    return res
